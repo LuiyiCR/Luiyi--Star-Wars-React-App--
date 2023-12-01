ac8782d9b45cac9b73fabde5e896b7a130d57914
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-12-01 01:07:35 +0000

    Map components

--- src/js/component/PlanetsCard.jsx
+++ src/js/component/PlanetsCard.jsx
@@ -1,48 +1,14 @@
 import React from 'react';
 
-export const PlanetCard = ({ name, image, description }) => (
-  <div className="Characters-container">
-    <h2>Planets!</h2>
-    <div className="card-group">
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title"></h5>
-          <p className="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </p>
-        </div>
-      </div>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This card has supporting text below as a natural lead-in to
-            additional content.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </p>
-        </div>
-      </div>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </p>
-        </div>
+export const Planet = ({ name, gender, hairColor, eyeColor }) => (
+  <div className="card-group">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{gender}</p>
+        <p className="card-text">{hairColor}</p>
+        <p className="card-text">{eyeColor}</p>
       </div>
     </div>
   </div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,86 @@
 import React, { useState } from 'react';
 import '../../styles/home.css';
-import { CharacterCard } from '../component/CharactersCard.jsx';
-import { PlanetCard } from '../component/PlanetsCard.jsx';
-import { VehicleCard } from '../component/VehiclesCard.jsx';
+import { Character } from '../component/CharactersCard.jsx';
+import { Planet } from '../component/PlanetsCard.jsx';
+import { Vehicle } from '../component/VehiclesCard.jsx';
 
 export const Home = () => {
   // const [characters, setCharacters] = useState([]);
+  // const [planets, setPlanets] = useState([]);
+  // const [vehicles, setVehicles] = useState([]);
+
+  const characters = [
+    {
+      id: 1,
+      name: 'Darth Vader',
+      gender: 'Gender: Male',
+      hairColor: 'Hair Color: Blond',
+      eyeColor: 'Eye Color: Blue',
+    },
+    { id: 2, name: 'Darth Vader', hairColor: 'Color: Blond', eyeColor: 'Blue' },
+  ];
+
+  const planets = [
+    {
+      id: 3,
+      name: 'Darth Vader',
+      gender: 'Gender: Male',
+      hairColor: 'Hair Color: Blond',
+      eyeColor: 'Eye Color: Blue',
+    },
+    { id: 4, name: 'Darth Vader', hairColor: 'Color: Blond', eyeColor: 'Blue' },
+  ];
+  const vehicles = [
+    {
+      id: 5,
+      name: 'Darth Vader',
+      gender: 'Gender: Male',
+      hairColor: 'Hair Color: Blond',
+      eyeColor: 'Eye Color: Blue',
+    },
+    { id: 6, name: 'Darth Vader', hairColor: 'Color: Blond', eyeColor: 'Blue' },
+  ];
 
   return (
-    <div className="card-container">
-      <CharacterCard /> <PlanetCard /> <VehicleCard />
+    <div className="characters-container">
+      <h2>Characters</h2>
+      <div className="card-container d-flex">
+        {characters.map((characterData, index) => (
+          <Character
+            key={characterData.id}
+            name={characterData.name}
+            gender={characterData.gender}
+            hairColor={characterData.hairColor}
+            eyeColor={characterData.eyeColor}
+          />
+        ))}
+      </div>
+
+      <h2>Planets</h2>
+      <div className="card-container d-flex">
+        {planets.map((planetData, index) => (
+          <Planet
+            key={planetData.id}
+            name={planetData.name}
+            gender={planetData.gender}
+            hairColor={planetData.hairColor}
+            eyeColor={planetData.eyeColor}
+          />
+        ))}
+      </div>
+
+      <h2>Vehicles</h2>
+      <div className="card-container d-flex">
+        {vehicles.map((vehicleData) => (
+          <Vehicle
+            key={vehicleData.id}
+            name={vehicleData.name}
+            gender={vehicleData.gender}
+            hairColor={vehicleData.hairColor}
+            eyeColor={vehicleData.eyeColor}
+          />
+        ))}
+      </div>
     </div>
   );
 };
