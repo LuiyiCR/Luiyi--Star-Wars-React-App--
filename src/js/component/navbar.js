707ba0af6a26c92c182d4a848db3c5bb7e3434f3
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-12-01 01:25:41 +0000

    Ajust Nav-bar, includes logo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/">
+        <img
+          src="https://download.logo.wine/logo/Star_Wars/Star_Wars-Logo.wine.png"
+          alt="Star Wars Logo"
+          className="navbar-brand logo-img"
+        />
+      </Link>
+      <div className="ml-auto">
+        <Link to="/demo">
+          <button className="btn btn-primary">Favorites</button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
