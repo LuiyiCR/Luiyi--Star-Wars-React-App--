babf1c3671fa5b233a5b5872197d922a63eef0ba
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-12-02 18:13:35 +0000

    Add counter in navbar for favorite button from Characters-Planets-Vehicles

--- src/js/component/CharactersCard.jsx
+++ src/js/component/CharactersCard.jsx
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../store/appContext';
 
-export const Character = ({ name, id }) => (
-  <div className="card" style={{ width: '18rem' }}>
-    <img
-      src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-      className="card-img-top"
-      alt="Luke SkyWalker"
-      onError={(e) => {
-        e.target.src =
-          'https://starwars-visualguide.com/assets/img/placeholder.jpg';
-      }}
-    />
-    <div className="card-body">
-      <h5 className="card-title">{name}</h5>
-      <div className="detail-fav-btn">
-        <Link to={`/caracter-details/${id}`}>
-          <button className="btn btn-outline-primary">
-            Character Details!
-          </button>
-        </Link>
-        <button
-          className="btn btn-outline-danger
+export const Character = ({ name, id }) => {
+  const { actions } = useContext(Context);
+
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+      <img
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+        className="card-img-top"
+        alt="Luke SkyWalker"
+        onError={(e) => {
+          e.target.src =
+            'https://starwars-visualguide.com/assets/img/placeholder.jpg';
+        }}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <div className="detail-fav-btn">
+          <Link to={`/caracter-details/${id}`}>
+            <button className="btn btn-outline-primary">
+              Character Details!
+            </button>
+          </Link>
+          <button
+            className="btn btn-outline-danger
         "
-          onClick={() => {
-            actions.incrementFavoritesCounter();
-            console.log('You picked a favorite!!');
-          }}
-        >
-          <i className="fa-regular fa-heart"></i>
-        </button>
+            onClick={() => {
+              actions.incrementFavoritesCounter();
+              console.log('You picked a favorite!!');
+            }}
+          >
+            <i className="fa-regular fa-heart"></i>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/js/component/PlanetsCard.jsx
+++ src/js/component/PlanetsCard.jsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../store/appContext';
 
-export const Planet = ({ name, id }) => (
-  <div className="card-group">
-    <div className="card" style={{ width: '18rem' }}>
-      <img
-        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        className="card-img-top"
-        alt="..."
-        onError={(e) => {
-          e.target.src =
-            'https://starwars-visualguide.com/assets/img/placeholder.jpg';
-        }}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <div className="detail-fav-btn">
-          <Link to={`/planet-details/${id}`}>
-            <button className="btn btn-outline-primary">Planet Details!</button>
-          </Link>
-          <button
-            className="btn btn-outline-danger
+export const Planet = ({ name, id }) => {
+  const { actions } = useContext(Context);
+
+  return (
+    <div className="card-group">
+      <div className="card" style={{ width: '18rem' }}>
+        <img
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+          className="card-img-top"
+          alt="..."
+          onError={(e) => {
+            e.target.src =
+              'https://starwars-visualguide.com/assets/img/placeholder.jpg';
+          }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <div className="detail-fav-btn">
+            <Link to={`/planet-details/${id}`}>
+              <button className="btn btn-outline-primary">
+                Planet Details!
+              </button>
+            </Link>
+            <button
+              className="btn btn-outline-danger
           "
-          >
-            <i className="fa-regular fa-heart"></i>
-          </button>
+              onClick={() => {
+                actions.incrementFavoritesCounter();
+                console.log('You picked a favorite!!');
+              }}
+            >
+              <i className="fa-regular fa-heart"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
