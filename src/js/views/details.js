df630f1e3546cce1be0549cd0d6ab6d68c057b9b
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-12-02 01:08:20 +0000

    Set detail-view

--- src/js/views/details.js
+++ src/js/views/details.js
@@ -5,23 +5,39 @@ import { Context } from '../store/appContext';
 
 import '../../styles/demo.css';
 
-export const Details = ({}) => (
-  <div className="card mb-3" style={{ maxWidth: '540px' }}>
+export const Details = ({
+  name,
+  gender,
+  eye_color,
+  hair_color,
+  skin_color,
+  height,
+}) => (
+  <div className="card mb-3 container" style={{ width: '45rem' }}>
     <div className="row g-0">
-      <div className="col-md-4">
-        <img src="..." className="img-fluid rounded-start" alt="..." />
+      <div className="col">
+        <img
+          src="https://lumiere-a.akamaihd.net/v1/images/din-djarin-main_a3810794.jpeg?region=143%2C37%2C1150%2C646"
+          className="img-fluid"
+          alt="..."
+        />
       </div>
-      <div className="col-md-8">
+      <div className="col">
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h2 className="card-title">Sky Walker{name}</h2>
           <p className="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            rem neque quidem.
           </p>
         </div>
+      </div>
+      <span className="border-divisor mt-5"></span>
+      <div className="container mt-1 text-danger">
+        <p className="card-text">Gender: Male{gender}</p>
+        <p className="card-text">Eye Color: Blue{eye_color}</p>
+        <p className="card-text">{hair_color}</p>
+        <p className="card-text">{skin_color}</p>
+        <p className="card-text">{height}</p>
       </div>
     </div>
   </div>
